ca8d6db6b7ae1c1f2cc4ae3b4de05b0f6c8fde84
Muhammad Ilham Nursastrawan <72774021+Deku242@users.noreply.github.com>
2023-06-15 01:09:46 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const PORT = process.env.PORT || 9000
 http
   .createServer((req, res) => {
     console.log('New connection')
-    res.end('Bismillah TCC A')
+    res.end('Prak Tcc 168 Cloud Run')
   })
   .listen(PORT, () => console.log('Listening on', PORT))
