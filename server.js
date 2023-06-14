4245368f2f6bb15b069118cf65a86547841edb15
Muhammad Ilham Nursastrawan <72774021+Deku242@users.noreply.github.com>
2023-06-15 01:44:38 +0700

    Update server.js

--- server.js
+++ server.js
@@ -3,6 +3,6 @@ const PORT = process.env.PORT || 9000
 http
   .createServer((req, res) => {
     console.log('New connection')
-    res.end('Hello world')
+    res.end('Hello World!')
   })
   .listen(PORT, () => console.log('Listening on', PORT))
